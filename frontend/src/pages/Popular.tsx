58325bce88e1f018378efc04aa1e9f67faed8086
hyejin na <920803hj@naver.com>
2025-05-11 21:58:33 +0900

    냉장고요리 수정중

--- frontend/src/pages/Popular.tsx
+++ frontend/src/pages/Popular.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import BottomNavBar from '../components/BottomNavBar';
+import TopNavBar from '../components/TopNavBar';
 
 const Popular = () => {
   return (
     <>
+      <TopNavBar />
       <div>Popular Page</div>
       <BottomNavBar activeTab="popularity" />
     </>
