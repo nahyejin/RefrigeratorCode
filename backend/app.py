f57a040746b5a8763ae4adeea165614a8fec8d70
hyejin na <920803hj@naver.com>
2025-05-19 16:03:32 +0900

    20개 제한없앰 이제 정렬필터 작업 시작해야함 250519_16:04

--- backend/app.py
+++ backend/app.py
@@ -19,7 +19,7 @@ def get_db():
 def get_recipes():
     db = get_db()
     cursor = db.cursor()
-    cursor.execute("SELECT * FROM recipes ORDER BY id DESC LIMIT 20")
+    cursor.execute("SELECT * FROM recipes ORDER BY id DESC")
     recipes = cursor.fetchall()
     db.close()
     return jsonify(recipes)
