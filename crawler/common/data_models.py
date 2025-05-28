32fdef5ec0f4c2ec0410d68600cac7ccb34d975c
hyejin na <920803hj@naver.com>
2025-05-28 14:34:37 +0900

    필터 디자인 수정 250528_14:35

--- crawler/common/data_models.py
+++ crawler/common/data_models.py
@@ -14,8 +14,10 @@ class Recipe:
     thumbnail: str
     likes: int
     comments: int
-    post_time: datetime
+    post_time: str  # Changed to str to store YYYY-MM-DD format
     platform: str
-    used_ingredients: List[str]
-    views: Optional[int] = None  # For YouTube videos
-    link: Optional[str] = None   # Original URL 
+    link: str
+    used_ingredients: Optional[List[str]] = None
+    used_ingredients_block: Optional[str] = None
+    block_reason: Optional[str] = None
+    views: Optional[int] = None  # For YouTube videos 
