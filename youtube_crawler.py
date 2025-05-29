807dbc5db180a1312004b1a4a5aa9bf8f9957190
hyejin na <920803hj@naver.com>
2025-05-30 07:31:27 +0900

    네이버 인플루언서 핫토픽 크롤러 조금 만지는 중 250530_7:31

---
+++ youtube_crawler.py
@@ -0,0 +1,7 @@
+import time
+import re
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+
+# ... existing code ... 
