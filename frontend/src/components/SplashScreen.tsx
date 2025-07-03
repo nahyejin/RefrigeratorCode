c6cc685a59c092af56c4f4684539ae26f9bfeb43
hyejin na <920803hj@naver.com>
2025-07-03 16:06:27 +0900

    스플래시 위치조정 250703_16:06

--- frontend/src/components/SplashScreen.tsx
+++ frontend/src/components/SplashScreen.tsx
@@ -37,7 +37,7 @@ const STYLES = {
   numberDisplay: {
     position: 'absolute' as const,
     left: '50%',
-    top: '73%',
+    top: '74%',
     transform: 'translate(-50%, 0)',
     color: '#FFA800',
     fontWeight: 700,
